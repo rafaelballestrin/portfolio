62145ceef6485eece59bbad1cf5ad7669c716812
Rafael <rafaelballestrin@gmail.com>
2019-02-13 21:46:17 +0000

    Add title and picture to Home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../Home/Home.css';
+import HomeBg from './Images/homeBgCropS.jpg';
 
 export default function Home() {
     return (
+        <div>
             <div>
-               <h3>Home content here</h3>
+                <h3>Hello, my name is Rafael. I am a Web Developer liviving in London, UK.</h3>
+                <img src={HomeBg} alt="Home" />
             </div>
+        </div>
     )
 }
