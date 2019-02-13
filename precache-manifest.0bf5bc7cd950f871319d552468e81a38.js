868f28dd07123a7d85949dcfa87e801315a6106a
Rafael <rafaelballestrin@gmail.com>
2019-02-13 21:14:50 +0000

    Updates

---
+++ precache-manifest.0bf5bc7cd950f871319d552468e81a38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "6bb887be46791c2bfe95",
+    "url": "/portfolio/static/js/main.6bb887be.chunk.js"
+  },
+  {
+    "revision": "6bcfea4a226050f173fa",
+    "url": "/portfolio/static/js/2.6bcfea4a.chunk.js"
+  },
+  {
+    "revision": "6bb887be46791c2bfe95",
+    "url": "/portfolio/static/css/main.45936e63.chunk.css"
+  },
+  {
+    "revision": "7e7603bd204eba6a054238b3876b0898",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ed6313640cebae7eefdae26305c82a00.js"
+  "/portfolio/precache-manifest.0bf5bc7cd950f871319d552468e81a38.js"
 );
 
 workbox.clientsClaim();
