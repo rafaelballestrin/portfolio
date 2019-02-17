c176dd27e9396e9e3f17ad124e0b8c50e13e78a6
Rafael <rafaelballestrin@gmail.com>
2019-02-17 21:23:31 +0000

    Updates

---
+++ precache-manifest.9c1f3e30d2b4fb92989b4184a61ca0a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea3d19dfeaf6d7f07ae4a9d4190679d1",
+    "url": "/portfolio/static/media/HomeImg.ea3d19df.jpg"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "563bf50dffc3a99f4446",
+    "url": "/portfolio/static/js/main.563bf50d.chunk.js"
+  },
+  {
+    "revision": "a1a24692c2707d25eb9e",
+    "url": "/portfolio/static/js/2.a1a24692.chunk.js"
+  },
+  {
+    "revision": "563bf50dffc3a99f4446",
+    "url": "/portfolio/static/css/main.7a235f08.chunk.css"
+  },
+  {
+    "revision": "34670952969b4bef10400214bf0a01db",
+    "url": "/portfolio/index.html"
+  }
+];
