5b3c89aefec6bfb977d062ebd780f1eb8f8840c7
Rafael <rafaelballestrin@gmail.com>
2019-02-17 21:45:52 +0000

    Add Projects Text

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default function Projects() {
     return (
-            <div>
-               <h3>Projects</h3>
-            </div>
+        <div>
+        <div>
+            <h3>Projects</h3>
+        </div>
+        <div>
+            <p>KODFLIX</p>
+            <p>Project based on re-create Netflix </p>
+        </div>
+        <div>
+            <p>KOINALYSIS</p>
+            <p>Team Project to display Crypto currencies prices </p>
+        </div>
+    </div>
     )
 }
