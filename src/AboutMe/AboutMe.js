78b7c63f211ba2ede2262a1cab378893a8f181ed
Rafael <rafaelballestrin@gmail.com>
2019-02-18 12:11:12 +0000

    FavIcon substituted for R

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -6,7 +6,7 @@ export default function AboutMe() {
             <div>
                 <h3>AboutMe</h3>
             </div>
-            <div>
+            <div className='content'>
                 <p>My interest in coding started in my early teenager years, when I self-taught HTML and created a fan website to my favourite rock band. After that I also created one website for a Volkswagen car’s dealer company. Years passed and I studied business and developed a career in marketing in global companies. Now I had the opportunity to go back to coding and I’m searching for an opportunity to join a dynamic and exciting team.</p>
             </div>
             <div>
